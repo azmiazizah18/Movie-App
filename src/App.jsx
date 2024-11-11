f4f89086bb9bfbf07fe1c8d5af5148c0fa698aa9
azmiazizah18 <nurazmidhimasazizah@gmail.com>
2024-11-11 23:25:50 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import Header from './component/Header';
+import Movie from './component/Movie';
+import './App.css';
+import axios from 'axios';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const API_KEY = '33b91e3b';  // API key 
+
+  const fetchMovies = async (query) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get(
+        `https://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`
+      );
+      console.log(response.data); // Log untuk debugging
+      if (response.data.Response === 'True') {
+        setMovies(response.data.Search);
+      } else {
+        setError(response.data.Error);
+      }
+    } catch (err) {
+      setError('Error fetching data');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies('batman');
+  }, []);
+
+  return (
+    <div className="app-container">
+      <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} onSearch={() => fetchMovies(searchTerm)} />
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      <div className="movie-grid">
+        {movies.map((movie) => (
+          <Movie key={movie.imdbID} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
